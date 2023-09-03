6bbf03097a0ed13b77ddcd7c43d6bce577983e89
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-09-03 12:56:03 -0700

    Database Connected

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
+
 
 async function connect () {
 
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/mongo-practice');
         console.log('Connected to MongoDB');
-    }
-
-    catch (error) {
+    
+    } catch (error) {
         console.log(error.message);
     }
 }
 
+
 connect();
+
+//---------------------------------------------
+
